5311e9d29d096de05ffc97e99f94a47e02c0bbe7
Afeez Mahmud <afeezmahmud@gmail.com>
2021-05-04 13:27:43 +0200

    Edited existing file and created New file

--- push.py
+++ push.py
@@ -2,6 +2,6 @@ print ("hello git")
 password = input ("Please Enter the passowrd! ")
 print ("change made in Master branch ")
 print ( " change made in hotfix branch" )
-
+print ("change made after DevnetUser forked" )
 
 
